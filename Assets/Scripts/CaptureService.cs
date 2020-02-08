ac78009ef3705aa68d5fcc774c7abd3ee71c4f43
Ladislao Lainez <thedragonladis@gmail.com>
2020-02-08 10:37:19 +0100

    Capture now fits correctly the size of the card.

--- Assets/Scripts/CaptureService.cs
+++ Assets/Scripts/CaptureService.cs
@@ -5,12 +5,16 @@ public class CaptureService
 {
     public async Task<Texture2D> CaptureCard(RectTransform card)
     {
-        int x = 0;
-        int y = 0;
-        int width = Screen.width;
-        int height = Screen.height;
-
+        int width = Mathf.CeilToInt(card.sizeDelta.x);
+        int height = Mathf.CeilToInt(card.sizeDelta.y);
+        int x = Mathf.CeilToInt(Screen.width * 0.5f + card.rect.x);
+        int y = Mathf.CeilToInt(Screen.height * 0.5f + card.rect.y);
+        
         var capturedRect = new Rect(x, y, width, height);
+
+        Debug.Log("Card rect " + card.rect);
+        Debug.Log("Captured rect " + capturedRect);
+        
         var texture = new Texture2D(width, height, TextureFormat.RGB24, false);
 
         await new WaitForEndOfFrame();
