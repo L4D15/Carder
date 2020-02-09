6beb9ab2fd880be7b4e2bfdf6da0d21c2efe034a
Ladislao Lainez <thedragonladis@gmail.com>
2020-02-09 17:44:31 +0100

    Parse markdown to convert it to rich text for TextMeshPro,

--- Assets/Becerra/Carder/Card/CardBodyTextView.cs
+++ Assets/Becerra/Carder/Card/CardBodyTextView.cs
@@ -1,4 +1,6 @@
-﻿using TMPro;
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using TMPro;
 using UnityEngine;
 
 namespace Becerra.Carder
@@ -9,11 +11,64 @@ namespace Becerra.Carder
         
         public void ShowText(string text)
         {
-            // TODO: Replace Markdown tags with BBCode tags
+            text = ConvertBold(text);
+            text = ConvertItalic(text);
             
             label.text = text;
             
             transform.SetAsLastSibling();
+        }
+
+        private string ConvertBold(string sourceText)
+        {
+            string result = sourceText;
+
+            var regex = new Regex(@"__.*?__");
+            var preMarkRegex = new Regex(@"__(?=.+?)");
+            var postMarkRegex = new Regex(@"(?<=.+?)__");
+            var matches = regex.Matches(sourceText);
+
+            foreach (Match match in matches)
+            {
+                string originalText = match.Value;
+                string convertedText = originalText;
+                
+                convertedText = preMarkRegex.Replace(convertedText, "<b>");
+                convertedText = postMarkRegex.Replace(convertedText, "</b>");
+                
+                result = result.Replace(originalText, convertedText);
+            }
+
+            return result;
+        }
+        
+        private string ConvertItalic(string sourceText)
+        {
+            string result = sourceText;
+
+            var regex = new Regex(@"\*\*.*?\*\*");
+            var preMarkRegex = new Regex(@"\*\*(?=.+?)");
+            var postMarkRegex = new Regex(@"(?<=.+?)\*\*");
+            var matches = regex.Matches(sourceText);
+
+            foreach (Match match in matches)
+            {
+                string originalText = match.Value;
+                string convertedText = originalText;
+                
+                convertedText = preMarkRegex.Replace(convertedText, "<i>");
+                convertedText = postMarkRegex.Replace(convertedText, "</i>");
+
+                result = result.Replace(originalText, convertedText);
+            }
+
+            return result;
+        }
+
+        private struct ConversionData
+        {
+            public int index;
+            public string text;
         }
     }
 }
